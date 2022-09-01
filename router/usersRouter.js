5080ebf24b22195c6eecce4f478797a62ab51976
bimashazaman <bimashazaman1234@gmail.com>
2022-09-02 05:28:36 +0600

    Middleware Validator

--- router/usersRouter.js
+++ router/usersRouter.js
@@ -4,12 +4,14 @@ const express = require("express");
 //internal Export
 const {getUsers} = require('../controller/usersController')
 const decorateHtmlResponse = require("../middlewares/common/decorateHtmlResponse");
-
+const avatarUpload = require('../middlewares/users/avatarUpload')
+const {addUserValidators, addUserValidationHandler} = require('../middlewares/users/userValidators')
 
 const router = express.Router();
 
 //Login page
 router.get("/", decorateHtmlResponse("Users"), getUsers);
+router.post("/", avatarUpload, addUserValidators);
 
 
 
