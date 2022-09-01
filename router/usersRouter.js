4e18909269014b4c1467b4f1a39c2c6ebad16b11
bimashazaman <bimashazaman1234@gmail.com>
2022-09-02 05:37:52 +0600

    Controller

--- router/usersRouter.js
+++ router/usersRouter.js
@@ -2,18 +2,30 @@
 const express = require("express");
 
 //internal Export
-const {getUsers} = require('../controller/usersController')
+const {
+  getUsers,
+  addUser,
+  removeUser,
+} = require("../controller/usersController");
 const decorateHtmlResponse = require("../middlewares/common/decorateHtmlResponse");
-const avatarUpload = require('../middlewares/users/avatarUpload')
-const {addUserValidators, addUserValidationHandler} = require('../middlewares/users/userValidators')
+const avatarUpload = require("../middlewares/users/avatarUpload");
+const {
+  addUserValidators,
+  addUserValidationHandler,
+} = require("../middlewares/users/userValidators");
 
 const router = express.Router();
 
 //Login page
 router.get("/", decorateHtmlResponse("Users"), getUsers);
-router.post("/", avatarUpload, addUserValidators);
-
-
+router.post(
+  "/",
+  avatarUpload,
+  addUserValidators,
+  addUserValidationHandler,
+  addUser,
+  removeUser
+);
 
 //export
 module.exports = router;
