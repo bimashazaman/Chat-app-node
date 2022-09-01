4d0784140793735e5f83aa188773b1dbdbb42e40
bimashazaman <bimashazaman1234@gmail.com>
2022-09-02 01:35:37 +0600

    Base

--- app.js
+++ app.js
@@ -1,5 +1,39 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const app = express();
+const mongoose = require("mongoose");
+const path = require("path");
+const cookieParser = require("cookie-parser");
 
+const app = express();
 dotenv.config();
+
+//db connection
+mongoose
+  .connect(process.env.MONGO_CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected"))
+  .catch((err) => console.log(err));
+
+//request parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//set the view engine
+app.set("view enhine", "ejs");
+
+// set the static folder
+app.use(express.static(path.join(__dirname, "public")));
+
+//parse cookies
+app.use(cookieParser(process.env.COOKIE_SECRET));
+
+//ROUTE setup
+
+//Error Handling
+
+//Port
+app.listen(process.env.PORT, () => {
+  console.log(`App listening on ${process.env.PORT}`);
+});
