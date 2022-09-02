0f421214766ed9052dbeba78da40851fff812806
bimashazaman <bimashazaman1234@gmail.com>
2022-09-02 12:04:08 +0600

    Controller

--- app.js
+++ app.js
@@ -1,56 +1,67 @@
-//External Import
+// external imports
 const express = require("express");
+const http = require("http");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const path = require("path");
 const cookieParser = require("cookie-parser");
+const moment = require("moment");
 
+// internal imports
+const loginRouter = require("./router/loginRouter");
+const usersRouter = require("./router/usersRouter");
+const inboxRouter = require("./router/inboxRouter");
 
-//Internal Import
-const {notFoundHandler, errorHandler} = require("./middlewares/common/errorHandler")
-const loginRouter = require('./router/loginRouter')
-const getUsers = require('./router/usersRouter')
-const getInbox = require('./router/inboxRouter')
-
+// internal imports
+const {
+  notFoundHandler,
+  errorHandler,
+} = require("./middlewares/common/errorHandler");
 
 const app = express();
+const server = http.createServer(app);
 dotenv.config();
 
-//db connection
+// socket creation
+const io = require("socket.io")(server);
+global.io = io;
+
+// set comment as app locals
+app.locals.moment = moment;
+
+// database connection
 mongoose
   .connect(process.env.MONGO_CONNECTION_STRING, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then(() => console.log("Connected"))
+  .then(() => console.log("database connection successful!"))
   .catch((err) => console.log(err));
 
-//request parser
+// request parsers
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-//set the view engine
-app.set("view enhine", "ejs");
+// set view engine
+app.set("view engine", "ejs");
 
-// set the static folder
+// set static folder
 app.use(express.static(path.join(__dirname, "public")));
 
-//parse cookies
+// parse cookies
 app.use(cookieParser(process.env.COOKIE_SECRET));
 
-//ROUTE setup
-app.use('/', loginRouter);
-app.use('/users', getUsers);
-app.use('/inbox', getInbox);
+// routing setup
+app.use("/", loginRouter);
+app.use("/users", usersRouter);
+app.use("/inbox", inboxRouter);
 
+// 404 not found handler
+app.use(notFoundHandler);
 
-//Error Handling
-app.use(notFoundHandler); //404
-app.use(errorHandler); //common
+// common error handler
+app.use(errorHandler);
 
-
-
-//Port
-app.listen(process.env.PORT, () => {
-  console.log(`App listening on ${process.env.PORT}`);
+server.listen(process.env.PORT, () => {
+  console.log(`app listening to port ${process.env.PORT}`);
 });
