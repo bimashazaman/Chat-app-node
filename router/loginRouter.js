e0ae68f1302f9ade37f37f5a37c3937baafa9074
bimashazaman <bimashazaman1234@gmail.com>
2022-09-02 06:59:52 +0600

    Controller

--- router/loginRouter.js
+++ router/loginRouter.js
@@ -1,14 +1,33 @@
-//External Import
+// external imports
 const express = require("express");
 
-//internal Export
-const { getLogin } = require("../controller/loginController");
+// internal imports
+const { getLogin, login, logout } = require("../controller/loginController");
 const decorateHtmlResponse = require("../middlewares/common/decorateHtmlResponse");
+const {
+  doLoginValidators,
+  doLoginValidationHandler,
+} = require("../middlewares/login/loginValidators");
+const { redirectLoggedIn } = require("../middlewares/common/checkLogin");
 
 const router = express.Router();
 
-//Login page
-router.get("/", decorateHtmlResponse("login"), getLogin);
+// set page title
+const page_title = "Login";
 
-//export
+// login page
+router.get("/", decorateHtmlResponse(page_title), redirectLoggedIn, getLogin);
+
+// process login
+router.post(
+  "/",
+  decorateHtmlResponse(page_title),
+  doLoginValidators,
+  doLoginValidationHandler,
+  login
+);
+
+// logout
+router.delete("/", logout);
+
 module.exports = router;
